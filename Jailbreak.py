4c5f2a14ba26bf47706a4833e495c70e00b512d9
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-05-06 19:15:33 +0300

    Create Jailbreak.py

---
+++ Jailbreak.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+The challenge is easy!
+Just exit this annoying code!
+Easy right?
+"""
+import signal
+from sys import exit as bye
+
+def why():
+    bye(0)
+
+def hmmm(sig,frame):
+    if sig == 8:
+        handler()
+    else:
+        print("\nLOL NOOB", end="")
+
+
+
+def handler(signal_received, frame):
+    bye(0)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, hmmm)
+    signal.signal(signal.SIGTSTP, hmmm)
+    signal.signal(signal.SIGQUIT, signal.SIG_IGN)
+    signal.signal(signal.SIGFPE, handler)
+    while True:
+        try:
+            var = input("Signal(Madness)>> ")
+            print(ord(var[-2]) // (ord(var[-15]) >> 1))
+        except(EOFError, IndexError):
+            print(end="\r")
+            print(end='\x1b[2K')
+            continue
+        except ZeroDivisionError:
+            why()
+        except Exception:
+            continue
