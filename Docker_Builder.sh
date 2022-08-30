721c736c875623043caccfdf2e9f0f2048ea72c9
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-08-30 13:26:49 +0300

    Add files via upload

--- Docker_Builder.sh
+++ Docker_Builder.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-set -xe
+#set -xe
 basedir=$(pwd)
 
-# Getting Directory names execluding current dir
-dirs=$(find $(pwd)/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
 
-Builder{  
-  privchk $1
-  for dir in "$dirs"
+Builder(){  
+  privchk $1 $2
+
+  # Getting Directory names execluding current dirs
+  dirs=$(find $(pwd)/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
+  
+  for dir in $dirs
   do
-    cd $dir
-    docker build -t $dir . 
-    aws ecr get-login-password \
-        | docker login 
-        --username AWS 
-        --password-stdin
-        $1
-      docker push "$dir"
-        # Returning to the base dir
-        cd $basedir
+    echo -e "\n[+] Building $dir"
+    cd "$dir"
+    docker build -t "$dir" . 
+    docker login --username AWS -p $(aws ecr get-login-password "$2") "$1"
+    docker push "$dir"
+    # Returning to the base dir
+    cd $basedir
   done
 }
 
-
-privchk{
+privchk(){
   chk=$(cat /etc/group | grep -e docker | grep -o $USER)
-  if [[ ! -f "$HOME/.aws/config" || ! -f "$HOME/.aws/credentials"]]; then 
+  if [[ ! -f "$HOME/.aws/config" || ! -f "$HOME/.aws/credentials" ]]; then 
     echo "[!] AWS credentials not found.... Aborting"
     exit 1
+  elif [[ "$1" == "help" ]]; then
+    echo "---> Usage: $0 <ECR Repo Link> <AWS Profile>"
+    exit 0
   elif [[ -z "$chk" ]]; then
     echo "[!] $USER isn't in docker group... aborting"
     exit 1
@@ -37,5 +38,10 @@ privchk{
   elif [[ -z "$1" ]];then
     echo "[!] Please specify ECR Repository link"
     exit 1
-  fi          
+  elif [[ -z "$2" ]]; then
+    echo "[WARNING] You didn't specify a profile argument for AWS"
+  fi
 }
+
+# Running
+Builder $1 $2
