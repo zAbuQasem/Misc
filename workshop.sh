d9107953f91fd4249f665736ba58399af81c3958
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-09-24 01:53:01 +0300

    Update workshop.sh

--- workshop.sh
+++ workshop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo docker pull ubuntu
+#sudo apt update
+#sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt update
+#apt-cache policy docker-ce
+#sudo apt install docker-ce -y
+#sudo docker pull ubuntu
+wget https://github.com/zAbuQasem/Misc/raw/main/workshop.tar.gz
+tar -zxvf workshop.tar.gz
+bash restart.sh
